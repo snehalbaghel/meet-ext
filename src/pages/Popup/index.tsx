f39bf21cc7cac6134780f73394b9de7644cd74bf
Snehal Baghel <snehalbaghel@gmail.com>
2020-11-22 12:33:07 +0530

    Add theme provider

--- src/pages/Popup/index.tsx
+++ src/pages/Popup/index.tsx
@@ -4,13 +4,33 @@ import { Provider } from 'react-redux';
 import { Store } from 'webext-redux';
 import Popup from './Popup';
 import './index.css';
+import { ThemeProvider } from 'styled-components';
 
 const store = new Store();
+
+const theme = {
+  blue: '#018CDE',
+  blueDark: '#318ECC',
+  blueDarker: '#006E9B',
+  blueLighter: '#E8F5FF',
+  commandRed: '#ef5e71',
+  errorRedLight: '#ffcdd2',
+  errorRed: '#E06F5D',
+  errorRedDark: '#CE4A50',
+  grey: '#D4D4D4',
+  greyDarkest: '#757575',
+  greyDarker: '#a5a5a5',
+  greyLight: '#ebebeb',
+  greyLighter: '#F2F3F3',
+  greyLightest: '#F0F0F0',
+};
 
 store.ready().then(() => {
   render(
     <Provider store={store}>
-      <Popup />
+      <ThemeProvider theme={theme}>
+        <Popup />
+      </ThemeProvider>
     </Provider>,
     window.document.querySelector('#app-container')
   );
